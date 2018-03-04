99e24e781869ed852c53af3aa0e058701efb10ee
jasonMisaq <powergame_coder2@yahoo.com>
2018-03-04 23:10:41 +0330

    Worked

--- app/src/main/java/me/mehdi/ffmpegsample/MainActivity.java
+++ app/src/main/java/me/mehdi/ffmpegsample/MainActivity.java
@@ -1,12 +1,16 @@
 package me.mehdi.ffmpegsample;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.hiteshsondhi88.libffmpeg.ExecuteBinaryResponseHandler;
@@ -16,21 +20,32 @@ import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunnin
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 100;
     private Context mContext = this;
     private Button run;
     private EditText command;
+    private TextView mResult;
+    private AlertDialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Handle permissions
+        if(ActivityCompat.checkSelfPermission(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(mContext, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
+        }
 
         command = findViewById(R.id.command);
         run = findViewById(R.id.run);
+        mResult = findViewById(R.id.result);
+        mDialog = new AlertDialog.Builder(mContext).create();
+
         run.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                runFFmpeg(new String[] {command.getText().toString()});
+                runFFmpeg(command.getText().toString().split(" "));
             }
         });
         FFmpeg ffmpeg = FFmpeg.getInstance(this);
@@ -56,6 +71,8 @@ public class MainActivity extends AppCompatActivity {
         } catch (FFmpegNotSupportedException e) {
             // Handle if FFmpeg is not supported by device
         }
+
+
 
          //Available methods: http://writingminds.github.io/ffmpeg-android-java/
 
@@ -83,21 +100,23 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void onProgress(String message) {
-                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+                    String old = mResult.getText().toString();
+                    mResult.setText(String.format("%s\n %s", old, message) );
                 }
 
                 @Override
                 public void onFailure(String message) {
-                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
-
+                    mDialog.setMessage(message);
+                    mDialog.show();
                 }
 
                 @Override
                 public void onSuccess(String message) {
                     Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_LONG).show();
-                    AlertDialog dialog = new AlertDialog.Builder(mContext).setMessage(message).setPositiveButton("OK", null).create();
-                    dialog.show();
+                    mDialog.setMessage(message);
+                    mDialog.show();
                 }
+
 
                 @Override
                 public void onFinish() {
